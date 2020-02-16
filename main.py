24ef356b02752d1644dad88952c058ed15a409f4
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2020-02-16 14:10:23 +0700

    Выбор предмета

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ import datetime
 bot = telebot.TeleBot('1073761581:AAF_Bf42Qit5PkRbrQbWf7yfprVogQMeemw')
 
 
+subjects = [
+    'Программирование параллельных процессов',
+    'Методы вычислений',
+    'Методология программной инженерии',
+    'Распределенные системы обработки информации',
+    'Конструирование компиляторов',
+    'Функциональные языки в разработки распределенных систем',
+    'Интелектуальные технологии обработки изображений',
+    'Научно-исследовательский семинар'
+]
+
+
+keyboard1 = telebot.types.ReplyKeyboardMarkup()
+for subject in subjects:
+    keyboard1.add(subject)
+
+
 def info(user, text):
     print(datetime.datetime.now(), "@" + user, text)
 
@@ -12,16 +29,12 @@ def info(user, text):
 @bot.message_handler(commands=['start'])
 def start_message(message):
     info(message.from_user.username, "/start")
-    bot.send_message(message.chat.id, 'Привет, это 3CRASBS!')
+    bot.send_message(message.chat.id, 'Привет, это 3CRASBS!', reply_markup=keyboard1)
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     info(message.from_user.username, message.text)
-    if message.text == 'Привет':
-        bot.send_message(message.chat.id, 'Привет, мой создатель')
-    elif message.text == 'Пока':
-        bot.send_message(message.chat.id, 'Прощай, создатель')
 
 
 bot.polling()
