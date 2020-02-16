4701b65d989a92862f2729282a7e72e652c51d9a
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2020-02-16 13:43:42 +0700

    Привет от 3CRABS

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-if __name__ == '__main__':
-    print("hello, world")
+import telebot
+
+bot = telebot.TeleBot('1073761581:AAF_Bf42Qit5PkRbrQbWf7yfprVogQMeemw')
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, это 3CRASBS!')
+
+
+bot.polling()
